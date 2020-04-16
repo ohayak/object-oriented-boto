36230872d538910a65c7c19584abaa35e644d27f
ohayak <ohayak@quantmetry.com>
2020-04-16 12:03:31 +0200

    Back to development: 0.0.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import Command, find_namespace_packages, setup
 # ----------------------------------------------------------------------------
 
 ROOT = os.path.dirname(__file__)
-VERSION = "0.0.6"
+VERSION = "0.0.7.DEV0"
 
 def file_contents(file_name):
     """Given a file name to a valid file returns the file object."""
