f54c1dc4468ed69f2cf9a826007ec5a528275e27
ohayak <ohayak@quantmetry.com>
2020-05-08 00:11:03 +0200

    fix data api

--- src/edfred/oob/rds/data_api.py
+++ src/edfred/oob/rds/data_api.py
@@ -1,15 +1,15 @@
-import aurora_data_api
+from aurora_data_api import AuroraDataAPIClient
 from .extentions import Base, MySQL
 
 
-class Connection(aurora_data_api.AuroraDataAPIClient):
+class Connection(AuroraDataAPIClient):
     def __init__(self, *args, **kwargs):
         super().__init__(
             dbname=kwargs["db"],
             aurora_cluster_arn=kwargs["aurora_cluster_arn"],
             secret_arn=kwargs["secret_arn"],
-            rds_data_client=kwargs["rds_data_client"],
-            charset=kwargs["charset"],
+            rds_data_client=kwargs.get("rds_data_client", None),
+            charset=kwargs.get("charset", None),
         )
 
 
