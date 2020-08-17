dc6ce6f719787557bb8c7593f276246e11e574f7
ohayak <ohayak@quantmetry.com>
2020-08-17 11:49:12 +0200

    fix glue job class

--- src/edfred/oob/glue/__init__.py
+++ src/edfred/oob/glue/__init__.py
@@ -1,10 +1,17 @@
 import sys
 import re
 import os
+from logging import Logger
 from typing import ClassVar, List, Dict
 from dataclasses import dataclass, field, asdict, InitVar
 from boto3 import Session
-from awsglue.utils import getResolvedOptions
+from awsglue.job import Job
+from awsglue.utils import getResolvedOptions, GlueArgumentError
+
+from awsglue.context import GlueContext
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
 
 
 @dataclass
@@ -16,6 +23,11 @@ class GlueJob:
     job_name: str = field(init=False)
     aws_region: str = field(init=False)
     aws_account_id: str = field(init=False)
+    spark_context: SparkContext = field(init=False)
+    glue_context: GlueContext = field(init=False)
+    spark_session: SparkSession = field(init=False)
+    glue_job: Job = field(init=False)
+    logger: Logger = field(init=False)
 
     def __post_init__(self):
         """Initialize the job."""
@@ -26,20 +38,26 @@ class GlueJob:
 
         args = " ".join(sys.argv[1:])
         options_names = map(lambda x: x.group(1), re.finditer(r"--([\d\w-]*)", args))
-        for option in options_names:
+        reserved_names = Job.continuation_options() + Job.job_bookmark_options() + Job.job_bookmark_range_options() + Job.id_params() + Job.encryption_type_options()
+        for option in set(options_names) - set(reserved_names):
             try:
                 self.arguments[option] = getResolvedOptions(sys.argv, [option])[option]
-            except KeyError:
+            except Exception as e:
+                print('Skip argument parsing Error:')
+                print(e)
                 continue
-            except RuntimeError as e:
-                if "Using reserved arguments" in str(e):
-                    continue
         self.job_name = getResolvedOptions(sys.argv, ["JOB_NAME"]).get("JOB_NAME", "")
         self.aws_region = self.environ.get("AWS_REGION", "")
         if "AWS_ACCOUNT_ID" not in self.environ:
             sts = Session().client("sts")
             self.environ["AWS_ACCOUNT_ID"] = sts.get_caller_identity()["Account"]
         self.aws_account_id = self.environ.get("AWS_ACCOUNT_ID", "")
+
+        self.spark_context = SparkContext()
+        self.glue_context = GlueContext(self.spark_context)
+        self.spark_session = self.glue_context.spark_session
+        self.glue_job = Job(self.glue_context)
+        self.logger = self.glue_context.get_logger()
 
     def main(self, *args, **kwargs):
         """Stub main method."""
